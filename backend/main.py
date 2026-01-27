aee9d6a48566b4558ea23750676a180cae0722fe
yoavuzan <yoav547@gmail.com>
2026-01-27 09:23:44 +0200

    change the regec CORS

--- backend/main.py
+++ backend/main.py
@@ -15,11 +15,12 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex=r"https://.*vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 game = Game()
 
